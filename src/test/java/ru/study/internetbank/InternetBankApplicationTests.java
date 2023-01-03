adb5363f7e67ed34cdbb7ee798a2f1b45ae03d61
Yuriy <urabasket98@gmail.com>
2023-01-03 17:38:27 +0500

    Initial commit

---
+++ src/test/java/ru/study/internetbank/InternetBankApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.study.internetbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternetBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
